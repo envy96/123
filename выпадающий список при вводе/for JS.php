90b118d43fe6b30874cd1f152daadf0224dc9838
envy96 <128578809+envy96@users.noreply.github.com>
2023-05-17 06:32:36 +0300

    Add files via upload

---
+++ выпадающий список при вводе/for JS.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="for%20JS.css">
+</head>
+<body>
+
+<div id="parent">
+    <input id="elem">
+    <ul id="list"></ul>
+</div>
+
+<script src="js1.js"></script>
+
+</body>
+</html>
+
